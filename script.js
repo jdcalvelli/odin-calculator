6ee67e3142669663ca2a5e7cb3925a1c1ae8c563
JD Calvelli <jdcalvelli@gmail.com>
2021-11-29 18:46:33 -0500

    add helper function operate

--- script.js
+++ script.js
@@ -14,3 +14,7 @@ function multiply(val1, val2) {
 function divide(val1, val2) {
   return val1 / val2;
 }
+
+function operate(operand1, operand2, operation) {
+  return operation(operand1, operand2);
+}
