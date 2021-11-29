86f415a9f0d15434f05ba12b63442d9b6515255a
JD Calvelli <jdcalvelli@gmail.com>
2021-11-29 18:41:49 -0500

    add basic arithmetic functions

---
+++ script.js
@@ -0,0 +1,16 @@
+//HELPER FUNCTIONS
+function add(val1, val2) {
+  return val1 + val2;
+}
+
+function subtract(val1, val2) {
+  return val1 - val2;
+}
+
+function multiply(val1, val2) {
+  return val1 * val2;
+}
+
+function divide(val1, val2) {
+  return val1 / val2;
+}
