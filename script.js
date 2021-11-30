2639fe5d5bcd805ea25f622d73add3028bd84e33
JD Calvelli <jdcalvelli@gmail.com>
2021-11-29 21:20:36 -0500

    wire up number button functionality

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 //DECLARATIONS
-let displayValue = 10;
+let displayValue = '0';
+
+let firstValue;
+let operation;
+let secondValue;
 
 //UI
 const calculatorDisplay = document.querySelector('.calculatorDisplay');
+
+const buttonList = document.querySelectorAll('button');
+
+
+//SETUP
+calculatorDisplay.textContent = displayValue;
+
+//LOGIC
+buttonList.forEach((button, index) => {
+  //check what index is, if its within the first bit, add event listener where it updatesDisplay to add on i+1 to displayValue
+  if (index < 9) {
+    button.addEventListener('click', () => {
+      if (displayValue == '0') {
+        displayValue = `${index + 1}`
+        calculatorDisplay.textContent = displayValue;
+      }
+      else {
+        displayValue += `${index + 1}`;
+        calculatorDisplay.textContent = displayValue;
+      }
+    });
+  }
+});
+
+
 
 //HELPER FUNCTIONS
 function add(val1, val2) {
